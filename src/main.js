b162e7b21307be5b15b2af9894384b9e39c1e8b1
Oka R. Abdillah <hans.abdillh05@gmail.com>
2022-06-03 17:14:05 +0800

    Update content

--- src/main.js
+++ src/main.js
@@ -1,50 +1,7 @@
 'use strict'
 
-// Get elements
-const articlesWrapper = document.querySelector('[data-role=articles-wrapper]')
+const navWrapper = document.querySelector('.nav-menu ul')
 
-// Render articles
-const articles = [
-  {
-    id: 1,
-	 title: 'Digital Wedding Invitation',
-	 description: 'Nam ac mauris at justo adipiscing facilisis. Nunc et velit. Donec.'
-  },
- {
-   id: 2,
-   title: 'Prewedding Videography',
-   description: 'Nam ac mauris at justo adipiscing facilisis. Nunc et velit. Donec.'
-  },
- {
-   id: 3,
-   title: 'Prewedding Photography',
-   description: 'Nam ac mauris at justo adipiscing facilisis. Nunc et velit. Donec.'
-  },
-  {
-   id: 4,
-   title: 'Wedding Organizer',
-   description: 'Nam ac mauris at justo adipiscing facilisis. Nunc et velit. Donec.'
-  }
-]
+const navs = ['Beranda', 'Produk', 'Testimoni', 'Kontak Kami']
 
-const createArticle = article => {
-  const { id, title, description } = article
-  return `
-	 <article class="article">
-      <div class="article-number">${ id }</div>
-      <div>
-		  <h1 class="article-title">${ title }</h1>
-		  <p class="article-description">${ description }</p>
-      </div>
-	 </article>
-  `
-}
-
-const renderArticles = articles => {
-  articlesWrapper.innerHTML = articles.map( article => createArticle(article)).join('')
-}
-
-// On load
-window.addEventListener('load', () => {
-  renderArticles(articles)
-})
+navWrapper.innerHTML = navs.map(nav => `<li>${ nav }</li>`).join('')
